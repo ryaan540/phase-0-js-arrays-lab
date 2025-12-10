ba8d7abb5ea9b29462798c7d6db11791d6fec108
ryaan540! <rkinyua841@gmail.com>
2025-12-10 08:51:59 +0300

    completed phase-0-js-arrays-lab

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,5 +1,39 @@
 // Write your code here
 
+let products = ["Laptop", "Phone", "Headphones", "Monitor"];
+
+
+// Log the first product in the array
+function logFirstProduct() {
+  console.log(products[0]);
+}
+logFirstProduct();
+
+
+// Add a new product "Tablet" to the end of the array
+function addProduct(productName) {
+  products.push(productName);
+}
+addProduct("Tablet");
+console.log(products);
+
+
+
+// Update the second product to "Smartphone",
+function updateProductName(index, newName) {
+  products[index] = newName;
+}
+updateProductName(1, "Smartphone");
+console.log(products);
+
+
+// Remove the last product from the array
+function removeLastProduct() {
+   let removed = products.pop();
+   console.log("Removed product:", removed);
+}
+removeLastProduct();
+console.log(products);
 
 
 // Export the necessary parts for testing
